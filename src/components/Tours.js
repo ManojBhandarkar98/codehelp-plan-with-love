c6f4f554c2fabdca4cc21e310329f779855dcf33
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-04 09:03:59 +0530

    Epi - 03 - Plan with Love - created all component

---
+++ src/components/Tours.js
@@ -0,0 +1,14 @@
+function Tours(props){
+
+    return (
+    <div>
+      <div>
+      <h2>Plan with Love</h2>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+    );
+}
+export default Tours;
