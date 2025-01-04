cd5602387a1c347593f4cf55d32f659f02cafaa2
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-04 16:02:15 +0530

    Epi - 03 - index.css style completed

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import Tours from "./components/Tours";
 
 const App = () => {
 
-  const [tours,setTours]= useState(data);
+  const [tours, setTours] = useState(data);
 
-  function removeTour(id){
-    const newTours = tours.filter(tour=>tour.id !== id);
+  function removeTour(id) {
+    const newTours = tours.filter(tour => tour.id !== id);
     setTours(newTours);
-}
- if(tours.length ===0){
+  }
+  if (tours.length === 0) {
+    return (
+      <div className="refresh">
+        <h2>No tours Left</h2>
+        <button className="btn-white" onClick={() => { setTours(data) }}>Refresh</button>
+      </div>
+    );
+  }
   return (
-    <div className="refresh">
-<h2>No tours Left</h2>
-<button onClick={()=>{setTours(data)}}>Refresh</button>
-    </div>
-  );
- }
-  return (
-    <div>
+    <div className="app">
       <Tours tours={tours} removeTour={removeTour}></Tours>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -12,12 +12,12 @@ function Card({ id, image, info, price, name, removeTour }) {
             <img src={image} className="image"></img>
             <div className="tour-info">
                 <div className="tour-details">
-                    <h4 className="tour-price">{price}</h4>
+                    <h4 className="tour-price">₹ {price}</h4>
                     <h4 className="tour-name">{name}</h4>
                 </div>
                 <div className="description">
                     {description}
-                    <span onClick={readmoreHandler}>{readmore ? 'showless' : 'readmore'}</span>
+                    <span className="read-more" onClick={readmoreHandler}>{readmore ? 'Show Less' : 'Read More'}</span>
                 </div>
             </div>
             <button onClick={()=>{removeTour(id)}} className="btn-red">Not Intrested</button>
