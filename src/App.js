c6ba1e709739ba4cc7169c4948458f30f21a3211
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-04 15:34:59 +0530

    Epi - 03 - Functionality completed of plan with love page

--- src/App.js
+++ src/App.js
@@ -6,9 +6,21 @@ const App = () => {
 
   const [tours,setTours]= useState(data);
 
+  function removeTour(id){
+    const newTours = tours.filter(tour=>tour.id !== id);
+    setTours(newTours);
+}
+ if(tours.length ===0){
+  return (
+    <div className="refresh">
+<h2>No tours Left</h2>
+<button onClick={()=>{setTours(data)}}>Refresh</button>
+    </div>
+  );
+ }
   return (
     <div>
-      <Tours Tours={data}></Tours>
+      <Tours tours={tours} removeTour={removeTour}></Tours>
     </div>
   );
 };
--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,14 +1,20 @@
-function Tours(props){
+import Card from "./Card";
 
-    return (
+function Tours({ tours, removeTour}) {
+
+  return (
     <div>
       <div>
-      <h2>Plan with Love</h2>
+        <h2>Plan with Love</h2>
       </div>
       <div>
-        
+        {
+          tours.map((tour)=>{
+            return <Card {...tour} removeTour={removeTour}> </Card>
+          })
+        }
       </div>
     </div>
-    );
+  );
 }
 export default Tours;
